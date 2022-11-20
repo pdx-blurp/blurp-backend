faf885d5b87fff7aee0d418f73c017628e26b335
AzerDime <guyian@pdx.edu>
2022-11-19 18:39:52 -0800

    added buttons redirecting and formatted files

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ejs = require('ejs')
 const app = express();
 
 //setting the view engine to ejs
@@ -8,4 +9,12 @@ app.get("/home", (req, res) => {
   //rendering index.ejs page
   res.render("index", { text: "hello world" });
 });
+
+const userRouter = require("./routes/landing")
+
+app.use("/landing", userRouter)
+
+app.get('/pg1ex', userRouter)
+app.get('/pg2ex', userRouter)
+
 app.listen(3000);
