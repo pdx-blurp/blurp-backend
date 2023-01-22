8c554256a265fd9ba619e3ae2150c75524641a96
daliaramos <dalia.ramoshernandez@gmail.com>
2023-01-21 18:00:09 -0800

    add /node and /node/get endpoints
    add /node/delete and /node/update endpoints
    
    finished /mode/update endpoint
    
    finished /mode/delete endpoint
    
    removed arrays used to test functions
    
    add status of response for /node/delete and /node/update
    
    changed /node/add to /node/create

--- routes/map.js
+++ routes/map.js
@@ -1,4 +1,4 @@
-let express = require('express');
+let express = require("express");
 const fs = require("fs");
 let router = express.Router();
 
@@ -6,8 +6,10 @@ let router = express.Router();
  * Root map endpoint, get request, returns all names including the team they belong to
  */
 
-router.get('/', (req, res, next) => {
-	res.send(JSON.parse(fs.readFileSync(__dirname + "/../public/devs.json", 'utf-8')));
+router.get("/", (req, res, next) => {
+  res.send(
+    JSON.parse(fs.readFileSync(__dirname + "/../public/devs.json", "utf-8"))
+  );
 });
 
 /**
@@ -15,30 +17,36 @@ router.get('/', (req, res, next) => {
  * The name and team must be specified to create a new entry.
  */
 
-router.post('/create', function (req, res, next) {
-    // Grab the parameters from the request body that we need
-    const {name, team} = req.body;
+router.post("/create", function (req, res, next) {
+  // Grab the parameters from the request body that we need
+  const { name, team } = req.body;
 
-    // Ensure name and team are specified
-    if (!name || !team) {
-        res.status(400).send("Must specify name and team!");
+  // Ensure name and team are specified
+  if (!name || !team) {
+    res.status(400).send("Must specify name and team!");
+  }
+
+  // This assumes the file already exists (and it should)
+  let json = JSON.parse(
+    fs.readFileSync(__dirname + "/../public/devs.json", "utf-8")
+  );
+
+  // Add name and team to JSON
+  json.push({ name, team });
+
+  // Write new changes to file
+  fs.writeFile(
+    __dirname + "/../public/devs.json",
+    JSON.stringify(json),
+    function (err) {
+      if (err) {
+        res.status(400).send("Issue writing file!");
+      }
     }
+  );
 
-    // This assumes the file already exists (and it should)
-    let json = JSON.parse(fs.readFileSync(__dirname + "/../public/devs.json", 'utf-8'));
-
-    // Add name and team to JSON
-    json.push({name, team});
-
-    // Write new changes to file
-    fs.writeFile(__dirname + "/../public/devs.json", JSON.stringify(json), function (err) {
-        if (err) {
-            res.status(400).send("Issue writing file!");
-        }
-    });
-
-    // Send JSON with new changes
-    res.send(json);
+  // Send JSON with new changes
+  res.send(json);
 });
 
 /**
@@ -47,7 +55,9 @@ router.post('/create', function (req, res, next) {
 
 router.get("/get", function (req, res, next) {
   const { name } = req.body;
-  let dev = JSON.parse(fs.readFileSync(__dirname + "/../public/devs.json", 'utf-8'));
+  let dev = JSON.parse(
+    fs.readFileSync(__dirname + "/../public/devs.json", "utf-8")
+  );
 
   const found = dev.find((devObj) => devObj.name === name);
   if (found) {
@@ -73,7 +83,7 @@ router.patch("/update", function (req, res, next) {
     Object.assign(inDB[found], update);
     fs.writeFile(__dirname + "/../public/tempDB.json", JSON.stringify(inDB), function (err) {
         if (err) {
-            res.status(400).send("Issue writing file!");
+          res.status(400).send("Issue writing file!");
         }
     });
     res.status(200).json(inDB[found]);
@@ -81,7 +91,6 @@ router.patch("/update", function (req, res, next) {
     res.status(404).json({ mess: "relationship not found" });
   }
 });
-
 
 /**
  * Delete endpoint, which takes unique relationshipID from the request body.
@@ -113,8 +122,72 @@ router.delete('/delete', function (req, res, next) {
         }
     });
 
-    // Send JSON with new changes
-    res.send(json);
+// Returns all the nodes in the map
+router.get("/node", (req, res) => {
+  res.send([
+    { nodeID: 1, userID: 1, mapID: 1, name: "Dalia", color: "Red" },
+    { nodeID: 2, userID: 1, mapID: 1, name: "Lili", color: "Green" },
+    { nodeID: 1, userID: 2, mapID: 2, name: "Dalia", color: "Red" },
+    { nodeID: 2, userID: 2, mapID: 2, name: "Lili", color: "Green" },
+  ]);
+});
+
+router.get("/node/get", (req, res) => {
+  const { userID } = req.body;
+
+  const found = nodeArr.find((node) => node.userID === userID);
+  if (found) {
+    const singleNode = nodeArr.filter((node) => node.userID === userID);
+    res.status(200).json(singleNode);
+  } else {
+    res.status(404).json({ message: "Node not found" });
+  }
+});
+
+router.post("/node/create", (req, res) => {
+  const { userID, mapID, name, color } = req.body;
+
+  nodeArr.push({ nodeID: 3, userID, mapID, name, color });
+  res.send(nodeArr);
+});
+
+router.delete("/node/delete", (req, res) => {
+  const { userID, mapID, nodeID } = req.body;
+
+  const found = false;
+  nodeArr.forEach((element, index) => {
+    if (
+      (element.nodeID === nodeID && element.userID === userID,
+      element.mapID === mapID)
+    ) {
+      nodeArr.splice(index, 1);
+      found = true;
+    }
+  });
+  if (found) {
+    res.status(200).json(nodeArr);
+  } else {
+    res.status(400).json({ message: "Node not found" });
+  }
+});
+
+router.patch("/node/update", (req, res) => {
+  const { userID, mapID, nodeID, name, color } = req.body;
+
+  nodeArr.forEach((element, index) => {
+    if (
+      (element.nodeID === nodeID && element.userID === userID,
+      element.mapID === mapID)
+    ) {
+      nodeArr[index].name = name;
+      nodeArr[index].color = color;
+    }
+  });
+  if (found) {
+    res.status(200).json(nodeArr);
+  } else {
+    res.status(400).json({ message: "Node not found" });
+  }
 });
 
 module.exports = router;
