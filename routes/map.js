4d6bae099ec613b57a04d695e8c7dbfa5efc36c2
AzerDime <guyian@pdx.edu>
2023-01-23 15:09:09 -0800

    refactor for tempDB instead of developers - rels

--- routes/map.js
+++ routes/map.js
@@ -59,53 +59,55 @@ router.get("/get", function (req, res, next) {
 });
 
 /**
- * Update endpoint, which takes the name parameter and updates the developer in the devs.json
+ * Update endpoint, which takes the relationshipID parameter and updates the relationship
+ * NOTE: "update" for this data structure should have some of (but not necessarily all of)
+ * nodeOne, nodeTwo, description, relationshipType
  */
 
 router.patch("/update", function (req, res, next) {
-  const { name, update } = req.body;
-  let dev = JSON.parse(fs.readFileSync(__dirname + "/../public/devs.json", 'utf-8'));
-  const found = dev.findIndex((devObj) => devObj.name === name);
+  const { relationshipID, update } = req.body;
+  let inDB = JSON.parse(fs.readFileSync(__dirname + "/../public/tempDB.json", 'utf-8'));
+  const found = inDB.findIndex((inDBobj) => inDBobj.relationshipID === relationshipID);
 
   if (found !== -1) {
-    Object.assign(dev[found], update);
-    fs.writeFile(__dirname + "/../public/devs.json", JSON.stringify(dev), function (err) {
+    Object.assign(inDB[found], update);
+    fs.writeFile(__dirname + "/../public/tempDB.json", JSON.stringify(inDB), function (err) {
         if (err) {
             res.status(400).send("Issue writing file!");
         }
     });
-    res.status(200).json(dev[found]);
+    res.status(200).json(inDB[found]);
   } else {
-    res.status(404).json({ mess: "developer not found" });
+    res.status(404).json({ mess: "relationship not found" });
   }
 });
 
 
 /**
- * Delete endpoint, which takes name and team parameters from the request body.
- * The name and team must be specified to remove zero or more entries, where multiple removals would occur when
- * multiple matches to the specified name and team parameters are present, or no removals if there are no matches.
+ * Delete endpoint, which takes unique relationshipID from the request body.
+ * Use a unique relationshipID to help increase specificity, which allows for 
+ * multiple removals if there are duplicates - or no removals if there are no matches.
  */
 router.delete('/delete', function (req, res, next) {
     // Grab the parameters from the request body that we need
-    const {name, team} = req.body;
+    const {relationshipID} = req.body;
 
-    // Ensure name and team are specified
-    if (!name || !team) {
-        res.status(400).send("Must specify name and team!");
+    // Ensure unique relationshipID is specified
+    if (!relationshipID) {
+        res.status(400).send("Must specify unique relationship ID!");
     }
 
     // This assumes the file already exists (and it should)
-    let json = JSON.parse(fs.readFileSync(__dirname + "/../public/devs.json", 'utf-8'));
+    let json = JSON.parse(fs.readFileSync(__dirname + "/../public/tempDB.json", 'utf-8'));
 
-    // Remove name and team from JSON
+    // Remove relationship with matching ID. Not entirely sure if written properly. 
     // This also removes duplicate instances
     json = json.filter(function (e) {
-        return e.name !== name && e.team !== team;
+        return e.relationshipID !== relationshipID;
     });
 
     // Write new changes to file
-    fs.writeFile(__dirname + "/../public/devs.json", JSON.stringify(json), function (err) {
+    fs.writeFile(__dirname + "/../public/tempDB.json", JSON.stringify(json), function (err) {
         if (err) {
             res.status(400).send("Issue writing file!");
         }
