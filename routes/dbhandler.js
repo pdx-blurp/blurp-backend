df72e34dedbed6978bb6ec0a4bb41d5f5ab6ed55
daliaramos <dalia.ramoshernandez@gmail.com>
2023-01-30 21:11:42 -0800

    added url to connect to mongodb

--- routes/dbhandler.js
+++ routes/dbhandler.js
@@ -1,22 +1,23 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
 // Connection URL
-const url = "mongodb+srv://mongoGroup:kLTsZ9EBUm7ErOpY@cluster0.fxl2nvt.mongodb.net/admin";
+const url =
+  "mongodb+srv://Blurp:Pdxgroupproject1!@cluster0.fxl2nvt.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'newMongoDB';
+const dbName = "newMongoDB";
 
 async function connect() {
-    // Use connect method to connect to the server
-    await client.connect();
-    console.log('Connected!');
-	return client;
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected!");
+  return client;
 }
 
 connect()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
 
 module.exports = { client };
