399dd31df845334a09e4749b54f89e439624ef4d
daliaramos <35042690+daliaramos@users.noreply.github.com>
2023-02-05 19:38:01 -0800

    Dramos/relationship (#44)
    * creating basic database connection code
    
    * wip database connection script
    
    * fixed syntax issue
    
    * added url to connect to mongodb
    
    * /create created a relationship document
    
    * finished / endpoint and started /delete
    
    * beginning corrections to relationship.js; remove test_dbhandler
    
    * WIP updating relationship route to query subdocument
    
    * wip: returning relationship info for specific relationship
    
    * relationship routes now interact with map format
    
    * abstracted connection url out of file; modified output string on relationship creation
    
    ---------
    
    Co-authored-by: jgmccarthy7 <jgm7@pdx.edu>
    Co-authored-by: jgm7 <jgm7@cecs.pdx.edu>

--- routes/relationship.js
+++ routes/relationship.js
@@ -1,152 +1,216 @@
-let express = require('express');
+let express = require("express");
 const fs = require("fs");
 let router = express.Router();
-const crypto = require('crypto');
-
+const crypto = require("crypto");
+const { client } = require("./dbhandler");
 /**
  * Create endpoint, which takes two node parameters to link together, as well as relationship info.
  */
-router.post('/create', function (req, res, next) {
-    // Grab the parameters from the request body that we need
-    const {node1, node2, relationship} = req.body;
+router.post("/create", function (req, res, next) {
+  // Grab the parameters from the request body that we need
+  const { mapID, relationshipinfo } = req.body;
 
-    // Ensure node1, node2, and relationship are specified
-    if (!node1 || !node2 || !relationship) {
-        res.status(400).send("Must specify node1, node2, and relationship!");
-        return;
-    }
+  // Ensure node1, node2, and relationship are specified
+  const relModel = [
+    'nodeOne', 'nodeTwo', 'description', 'type', 'stress', 'familiarity'
+  ]
+  if (!mapID) {
+    res.status(400).send("Must specify map!");
+    return;
+  }
+  else if (!(relationshipinfo && relModel.every((i) => relationshipinfo.hasOwnProperty(i)))) {
+    let responseString = "relationshipinfo must have these keys: " + relModel.join(', ');
+    res.status(400).send(responseString);
+    return;
+  }
+  //connects to the db
+  client
+    .connect()
+    .then((response) => {
+      const database = response.db("blurp");
+      const collection = database.collection("maps");
 
-    // This assumes the file already exists (and it should)
-    let json = JSON.parse(fs.readFileSync(__dirname + "/../public/relationships.json", 'utf-8'));
-
-    let id = crypto.randomUUID();
-
-    // Add to JSON
-    json.push({id, node1, node2, relationship});
-
-    // Write new changes to file
-    fs.writeFile(__dirname + "/../public/relationships.json", JSON.stringify(json), function (err) {
-        if (err) {
-            res.status(400).send("Issue writing file!");
+      let relationshipData = {
+        'relationshipID': crypto.randomUUID(),
+        'nodePair': {
+          'nodeOne': relationshipinfo.nodeOne,
+          'nodeTwo': relationshipinfo.nodeTwo,
+        },
+        'description': relationshipinfo.description,
+        'relationshipType': {
+          'type': relationshipinfo.type,
+          'stressCode': relationshipinfo.stress,
+          'familiarity': relationshipinfo.familiarity
         }
+      };
+
+      console.log(relationshipData);
+      console.log(mapID);
+
+      collection
+        .updateOne(
+          { mapID: mapID },
+          { $push: { "relationships": relationshipData}}
+        )
+        .then((output) => {
+          console.log(output);
+          if (output.modifiedCount === 1 && output.matchedCount === 1) {
+            res.status(200).send({ message: "Relationship created"});
+          }
+          else if (output.matchedCount === 0) {
+            res.status(400).send({ message: "Relationship not created"}); 
+          }
+        })
+        .catch((err) => {
+          res.status(400).send({ message: "Relationship was not created" });
+          console.log(err);
+        });
+    })
+    .catch((err) => {
+      res.status(400).send({ message: "Database not connected" });
     });
-
-    // Send JSON with new changes
-    res.send(json);
 });
-
 
 /**
  * Delete endpoint, which takes the relationship id.
  */
-router.delete('/delete', function (req, res, next) {
-    // Grab the parameters from the request body that we need
-    const {id} = req.body;
+router.delete("/delete", function (req, res, next) {
+  // Grab the parameters from the request body that we need
+  const { mapID, relationshipID } = req.body;
 
-    // Ensure id is specified
-    if (!id) {
-        res.status(400).send("Must specify id!");
-        return;
-    }
-
-    // This assumes the file already exists (and it should)
-    let json = JSON.parse(fs.readFileSync(__dirname + "/../public/relationships.json", 'utf-8'));
-
-    // Remove by id from json
-    // This also removes duplicate instances
-    json = json.filter(function (e) {
-        return e.id !== id;
+  // Ensure id is specified
+  if (!mapID || !relationshipID) {
+    res.status(400).send("Must specify map and relationship!");
+    return;
+  }
+  client.connect().then((response) => {
+    const database = client.db("blurp");
+    const collection = database.collection("maps");
+    collection.updateOne(
+      { mapID: mapID, "relationships.relationshipID": relationshipID},
+      { $pull: { "relationships": {"relationshipID": relationshipID}} }
+    )
+    .then((output) => {
+      if (output.modifiedCount === 1 && output.matchedCount === 1) {
+        res.status(200).send({ message: "Relationship deleted"});
+      }
+      else if (output.matchedCount === 0) {
+        res.status(400).send({ message: "Relationship not found"});
+      }
+    })
+    .catch((err) => {
+      res.status(400).send({ message: err });
+	    console.log(err);
     });
-
-    // Write new changes to file
-    fs.writeFile(__dirname + "/../public/relationships.json", JSON.stringify(json), function (err) {
-        if (err) {
-            res.status(400).send("Issue writing file!");
-        }
-    });
-
-    // Send JSON with new changes
-    res.send(json);
+  })
+  .catch((err) => {
+    res.status(400).send({ message: "Database not connected" });
+	  console.log(err);
+  });
 });
 
 /**
  * Update endpoint, which takes the relationship id and relationship data to update.
  */
-router.put('/update', function (req, res, next) {
-    // Grab the parameters from the request body that we need
-    const {id, relationship} = req.body;
+router.put("/update", function (req, res, next) {
+  // Grab the parameters from the request body that we need
+  const { mapID, relationshipID, changes } = req.body;
 
-    // Ensure id is specified
-    if (!id || !relationship) {
-        res.status(400).send("Must specify id and relationship!");
-        return;
+  // Ensure id is specified
+  if (!mapID || !relationshipID) {
+    res.status(400).send("Must specify map and relationship!");
+    return;
+  }
+
+
+  client.connect().then((response) => {
+    //const found = false;
+    const database = client.db("blurp");
+    const collection = database.collection("maps");
+    let updates = {};
+    for (const [k, v] of Object.entries(changes)) {
+      updates['relationships.$[rel].' + k] = v;
     }
-
-    // This assumes the file already exists (and it should)
-    let json = JSON.parse(fs.readFileSync(__dirname + "/../public/relationships.json", 'utf-8'));
-
-    // Grab the record to update
-    let recordToUpdate = json.filter(function (e) {
-        return e.id === id;
-    })[0];
-    // Remove the record by id to add later
-    json = json.filter(function (e) {
-        return e.id !== id;
+    collection.updateOne(
+      { mapID: mapID, "relationships.relationshipID": relationshipID},
+      { $set: updates },
+      { arrayFilters: [
+          {"rel.relationshipID": relationshipID}
+      ]}
+    )
+    .then((output) => {
+      if (output.modifiedCount === 1 && output.matchedCount === 1) {
+        res.status(200).send({ message: "Relationship updated"});
+      }
+      else if (output.matchedCount === 0) {
+        res.status(400).send({ message: "Relationship not found"});
+      }
+    })
+    .catch((err) => {
+      res.status(400).send({ message: "Database not connected" });
+	  console.log(err);
     });
-
-    recordToUpdate.relationship = relationship;
-    let node1 = recordToUpdate.node1;
-    let node2 = recordToUpdate.node2;
-
-    // Add name and team to JSON
-    json.push({id, node1, node2, relationship});
-
-    // Write new changes to file
-    fs.writeFile(__dirname + "/../public/relationships.json", JSON.stringify(json), function (err) {
-        if (err) {
-            res.status(400).send("Issue writing file!");
-        }
-    });
-
-    // Send JSON with new changes
-    res.send(json);
+  // This assumes the file already exists (and it should)
+  });
 });
-
 
 /**
  * Get endpoint, which takes a single relationship id.
  */
-router.get('/get', function (req, res, next) {
-    // Grab the parameters from the request body that we need
-    const {id} = req.body;
+router.get("/get", function (req, res, next) {
+  // Grab the parameters from the request body that we need
+  const { mapid, relid } = req.body;
 
-    // Ensure id is specified
-    if (!id) {
-        res.status(400).send("Must specify id!");
-        return;
-    }
-
-    // This assumes the file already exists (and it should)
-    let json = JSON.parse(fs.readFileSync(__dirname + "/../public/relationships.json", 'utf-8'));
-
-    // Grab the record to update
-    let matchedRecord = json.filter(function (e) {
-        return e.id === id;
-    })[0];
-
-    // Send matched JSON
-    res.send(matchedRecord);
+  // Ensure id is specified
+  if (!mapid || !relid) {
+    res.status(400).send("Must specify mapid & relid!");
+    return;
+  }
+  client
+    .connect()
+    .then((response) => {
+      const database = response.db("blurp");
+      const collection = database.collection("maps");
+		collection.aggregate([
+			{ $match: {mapID: '8734873454567456to9'} },
+  			{ $unwind: "$relationships" },
+  			{ $match: { 'relationships.description': "Wife"}},
+  			{ $project: { relationships: 1, _id:0}}	
+		])
+		.toArray()
+		.then(output => {
+			console.log(output)
+			res.send(output)
+		});
+    })
+    .catch((err) => {
+      res.status(400).send({ message: "Database not connected" });
+	  console.log(err);
+    });
 });
 
 /**
  * Root endpoint, which sends all relationships.
  */
-router.get('/', function (req, res, next) {
-    // This assumes the file already exists (and it should)
-    let json = JSON.parse(fs.readFileSync(__dirname + "/../public/relationships.json", 'utf-8'));
+router.get("/", function (req, res, next) {
+  const { mapid } = req.body;
+  client
+    .connect()
+    .then((response) => {
+      const database = response.db("blurp");
+      const collection = database.collection("maps");
 
-    // Send all JSON
-    res.send(json);
+      collection.find({mapID: String(mapid)})
+		.project({relationships:1, _id:0})
+		.toArray(function (err, result) {
+        if (err) throw err;
+        res.send(result);
+        //database.close();
+      });
+    })
+    .catch((err) => {
+      res.status(400).send({ message: "Database not connected" });
+    });
 });
 
 module.exports = router;
