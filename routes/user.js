40f8be423a798f62f052bb3a5811dfb620689cf0
ngotruc999 <ngotruc@pdx.edu>
2023-02-07 14:00:58 -0800

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,33 @@ const express =  require('express');
 // controlle functions
 const { signupUser, loginUser } = require('../controllers/userController')
 
-const routes = express.Router();
+const router = express.Router();
 
 // login route
 router.post('/login', loginUser)
 
+// get a single user
+router.get('/:id', (req,res) => {
+    res.json({mssg: "GET a single user"});
+})
+
+// POST a user
+router.post('/', (req,res) => {
+    res.json({mssg: "POST a user"});
+});
+
+// DELETE a user
+router.delete('/:id', (req,res) => {
+    res.json({mssg: "DELETE a user"});
+});
+
+// Update a user
+router.patch('/:id', (req,res) => {
+    res.json({mssg: "Update a user"});
+});
 
 //signup route
 router.post('/signup', signupUser)
 
 
-module.exports = routes
+module.exports = router
