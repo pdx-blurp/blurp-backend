f6280ccc908c8608f144f48b3c885f03bab1eda1
Thomas <trg5@pdx.edu>
2023-02-13 12:00:36 -0800

    Added cors to the backend as the frontend was getting errors regarding CORS requests being blocked, and added a few lines in each spot where the router was being set up. Also changed the db path and added the db file to the gitignore so it doesn't accidentally get committed

--- routes/landing.js
+++ routes/landing.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+/* 
+Using this to prevent the CORS blocked message that was popping up on front-end
+https://stackoverflow.com/questions/58403651/react-component-has-been-blocked-by-cors-policy-no-access-control-allow-origin
+*/
+const cors = require("cors");
+router.use(cors());
+
 router.get("/pg1ex", (req, res) => {
-	res.render('./pg1ex')
-})
+	res.render("./pg1ex");
+});
 
 router.get("/pg2ex", (req, res) => {
-	res.render('./pg2ex')
-})
+	res.render("./pg2ex");
+});
 
 module.exports = router;
