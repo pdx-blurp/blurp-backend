1e5e0799a5beaa561faa549185f3bce2ab8ecffb
trg5pdx <47119853+trg5pdx@users.noreply.github.com>
2023-02-23 11:17:01 -0800

    Thomasg/load map id post changes: added cors, added size to nodes/relationships, etc (#59)
    * root, get, update, delete routes done
    
    * create route added
    
    * map/create now adds a groups list
    
    * Added cors to the backend as the frontend was getting errors regarding CORS requests being blocked, and added a few lines in each spot where the router was being set up. Also changed the db path and added the db file to the gitignore so it doesn't accidentally get committed
    
    * Set up the db to take in relationship and node id's from the front end rather than generating them, and set up the backend to send back what the map id is for the frontend
    
    * add relationship and node size
    
    * Set up the db to take in relationship and node id's from the front end rather than generating them, and set up the backend to send back what the map id is for the frontend
    
    * removed console.logs that were being used for development
    
    ---------
    
    Co-authored-by: jgm7 <jgm7@cecs.pdx.edu>
    Co-authored-by: AzerDime <guyian@pdx.edu>

--- routes/dbhandler.js
+++ routes/dbhandler.js
@@ -2,22 +2,22 @@ const { MongoClient } = require("mongodb");
 const fs = require("fs");
 
 // Connection URL
-const url = fs.readFileSync(__dirname + "/../../mongo.db", "utf-8");
+const url = fs.readFileSync(__dirname + "/../mongo.db", "utf-8");
 const client = new MongoClient(url);
 
 // Database Name
 const dbName = "newMongoDB";
 
 async function connect() {
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log("Connected!");
-  return client;
+	// Use connect method to connect to the server
+	await client.connect();
+	console.log("Connected!");
+	return client;
 }
 
 connect()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+	.then(console.log)
+	.catch(console.error)
+	.finally(() => client.close());
 
 module.exports = { client };
--- routes/node.js
+++ routes/node.js
@@ -1,26 +1,33 @@
 let express = require("express");
 let router = express.Router();
-const crypto = require('crypto');
+const crypto = require("crypto");
 const fs = require("fs");
 
-const {MongoClient} = require("mongodb");
+/* 
+Using this to prevent the CORS blocked message that was popping up on front-end
+https://stackoverflow.com/questions/58403651/react-component-has-been-blocked-by-cors-policy-no-access-control-allow-origin
+*/
+const cors = require("cors");
+router.use(cors());
+
+const { MongoClient } = require("mongodb");
 
 // Connection URL
-const url = fs.readFileSync(__dirname + "/../../mongo.db", "utf-8");
+const url = fs.readFileSync(__dirname + "/../mongo.db", "utf-8");
 const client = new MongoClient(url);
 
 /**
  * GET request which takes mapID and gets all nodes associated with it.
  */
 router.get("/", (req, res) => {
-	const {mapID} = req.body;
+	const { mapID } = req.body;
 
 	if (!mapID) {
 		res.status(400).send("mapID must be specified!");
 		return;
 	}
 
-	client.connect(err => {
+	client.connect((err) => {
 		if (err) {
 			throw err;
 		}
@@ -28,8 +35,9 @@ router.get("/", (req, res) => {
 		const db = client.db("blurp");
 		const collection = db.collection("maps");
 
-		collection.find({mapID: mapID})
-			.project({nodes: 1, _id:0})
+		collection
+			.find({ mapID: mapID })
+			.project({ nodes: 1, _id: 0 })
 			.toArray()
 			.then((result) => {
 				res.status(200).json(result);
@@ -41,14 +49,14 @@ router.get("/", (req, res) => {
  * GET request which takes a nodeID and returns its data.
  */
 router.get("/get", (req, res) => {
-	const {mapID, nodeID} = req.body;
+	const { mapID, nodeID } = req.body;
 
 	if (!nodeID || !mapID) {
 		res.status(400).send("nodeID and mapID must be specified!");
 		return;
 	}
 
-	client.connect(err => {
+	client.connect((err) => {
 		if (err) {
 			throw err;
 		}
@@ -56,22 +64,17 @@ router.get("/get", (req, res) => {
 		const db = client.db("blurp");
 		const collection = db.collection("maps");
 
-		collection.aggregate([
-			{$match: {mapID: mapID}},
-			{ $unwind: "$nodes"},
-			{ $match: {'nodes.nodeID': nodeID}},
-			{ $project: {nodes: 1, _id:0}}
-		])
-		.toArray()
-		.then(output => {
-			if ( output.length <= 0 ) {
-				res.status(400).send("Node not found")
-			}
-			else {
-				output = output[0].nodes;
-				res.status(200).json(output);
-			}
-		});
+		collection
+			.aggregate([{ $match: { mapID: mapID } }, { $unwind: "$nodes" }, { $match: { "nodes.nodeID": nodeID } }, { $project: { nodes: 1, _id: 0 } }])
+			.toArray()
+			.then((output) => {
+				if (output.length <= 0) {
+					res.status(400).send("Node not found");
+				} else {
+					output = output[0].nodes;
+					res.status(200).json(output);
+				}
+			});
 	});
 });
 
@@ -79,51 +82,47 @@ router.get("/get", (req, res) => {
  * POST request which takes userID, mapID, name, and color, and creates a new node record.
  */
 router.post("/create", (req, res) => {
-	const {userID, mapID, nodeinfo} = req.body;
+	const { userID, mapID, nodeinfo } = req.body;
 
 	if (!userID || !mapID || !nodeinfo) {
-		res.status(400).send("userID, mapID, nodeinfo must be specified!")
+		res.status(400).send("userID, mapID, nodeinfo must be specified!");
 		return;
 	}
 
-	client.connect(err => {
+	client.connect((err) => {
 		if (err) {
 			throw err;
 		}
-
 		const db = client.db("blurp");
 		const collection = db.collection("maps");
 
 		const newEntry = {
-			nodeName: nodeinfo.nodeName, 
-			nodeID: crypto.randomUUID(), 
-			color: nodeinfo.color,
+			nodeName: nodeinfo.nodeName,
+			nodeID: nodeinfo.nodeID,
 			description: nodeinfo.description,
 			pos: {
 				x: nodeinfo.pos.x,
-				y: nodeinfo.pos.y
+				y: nodeinfo.pos.y,
 			},
 			type: nodeinfo.type,
 			age: nodeinfo.age,
 			color: nodeinfo.color,
-			groups: []
+			groups: [],
+			size: nodeinfo.size,
 		};
 		//collection.insertOne(newEntry)
-		collection.updateOne(
-			{ mapID: mapID },
-			{ $push: { "nodes": newEntry}}
-		)
-		.then((output) => {
-			if (output.modifiedCount === 1 && output.matchedCount === 1) {
-			  res.status(200).send({ message: "Node created"});
-			}
-			else if (output.matchedCount === 0) {
-			  res.status(400).send({ message: "Node not created"}); 
-			}
-		})
-		.catch(error => {
-				res.status(400).send("Invalid input.")
-		})
+		collection
+			.updateOne({ mapID: mapID }, { $push: { nodes: newEntry } })
+			.then((output) => {
+				if (output.modifiedCount === 1 && output.matchedCount === 1) {
+					res.status(200).send({ message: "Node created" });
+				} else if (output.matchedCount === 0) {
+					res.status(400).send({ message: "Node not created" });
+				}
+			})
+			.catch((error) => {
+				res.status(400).send("Invalid input.");
+			});
 	});
 });
 
@@ -131,33 +130,33 @@ router.post("/create", (req, res) => {
  * DELETE request which takes a nodeID to delete and a mapID to ensure it is deleted from the correct map.
  */
 router.delete("/delete", (req, res) => {
-	const {mapID, nodeID} = req.body;
+	const { mapID, nodeID } = req.body;
 
 	if (!nodeID || !mapID) {
 		res.status(400).send("nodeID and mapID must be specified!");
 		return;
 	}
 
-	client.connect(err => {
+	client.connect((err) => {
 		if (err) {
 			throw err;
 		}
 
 		const db = client.db("blurp");
 		const collection = db.collection("maps");
-	// Remove the node from map, and any relationships connected to the map
-		collection.updateOne(
-			{ mapID: mapID, "nodes.nodeID": nodeID},
-			{ $pull: {"nodes": {"nodeID": nodeID}, "relationships": {$or: [ {"nodePair.nodeOne": nodeID}, {"nodePair.nodeTwo": nodeID}]} }}
-		)
-		.then((output) => {
-			if (output.modifiedCount === 1 && output.matchedCount === 1) {
-				res.status(200).send({ message: "Node deleted"});
-		    }
-			else if (output.matchedCount === 0) {
-				res.status(400).send({ message: "Node not found"});
-		    }
-		});
+		// Remove the node from map, and any relationships connected to the map
+		collection
+			.updateOne(
+				{ mapID: mapID, "nodes.nodeID": nodeID },
+				{ $pull: { nodes: { nodeID: nodeID }, relationships: { $or: [{ "nodePair.nodeOne": nodeID }, { "nodePair.nodeTwo": nodeID }] } } }
+			)
+			.then((output) => {
+				if (output.modifiedCount === 1 && output.matchedCount === 1) {
+					res.status(200).send({ message: "Node deleted" });
+				} else if (output.matchedCount === 0) {
+					res.status(400).send({ message: "Node not found" });
+				}
+			});
 	});
 });
 
@@ -166,14 +165,14 @@ router.delete("/delete", (req, res) => {
  * be updated with the new name and color specified.
  */
 router.patch("/update", (req, res) => {
-	const {nodeID, mapID, changes} = req.body;
+	const { nodeID, mapID, changes } = req.body;
 
 	if (!nodeID || !mapID || !changes) {
 		res.status(400).send("nodeID, mapID, and changes must be specified!");
 		return;
 	}
 
-	client.connect(err => {
+	client.connect((err) => {
 		if (err) {
 			throw err;
 		}
@@ -181,21 +180,13 @@ router.patch("/update", (req, res) => {
 		const collection = db.collection("maps");
 		let updates = {};
 		for (const [k, v] of Object.entries(changes)) {
-			updates['nodes.$[node].' + k] = v;
+			updates["nodes.$[node]." + k] = v;
 		}
-		collection.updateOne(
-			{mapID: mapID, "nodes.nodeID": nodeID},
-			{$set: updates},
-			{arrayFilters: [
-				{"node.nodeID": nodeID}
-			]}
-		)
-		.then((output) => {
+		collection.updateOne({ mapID: mapID, "nodes.nodeID": nodeID }, { $set: updates }, { arrayFilters: [{ "node.nodeID": nodeID }] }).then((output) => {
 			if (output.modifiedCount === 1 && output.matchedCount === 1) {
-				res.status(200).send({ message: "Node updated"});
-			}
-		    else if (output.matchedCount === 0) {
-				res.status(400).send({ message: "Node not found"});
+				res.status(200).send({ message: "Node updated" });
+			} else if (output.matchedCount === 0) {
+				res.status(400).send({ message: "Node not found" });
 			}
 		});
 	});
