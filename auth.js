40f49308d16bc42c769d7c28c601298265fe75a1
ngotruc999 <ngotruc@pdx.edu>
2023-02-25 11:36:03 -0800

    connect mongodb

--- auth.js
+++ auth.js
@@ -1,31 +1,66 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { client } = require("./routes/dbhandler")
 
 const GOOGLE_CLIENT_ID = '220935592619-e7j93usk2h7vhcuoauos59rhgvqlcmsa.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = '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';
+
+//passport.use(new GoogleStrategy({
+    //clientID: GOOGLE_CLIENT_ID,
+    //clientSecret: GOOGLE_CLIENT_SECRET,
+    //callbackURL: "http://localhost:5500/google/callback"
+    //},
+    //function(accessToken, refreshToken, profile, done) {
+        // check if user is already exist in db
+        //db.collection('account').findOne({authID: profile.id}).then((currentUser) => {
+            //if (currentUser) {
+                //console.log('user is:', currentUser);
+                //done(null, currentUser);
+            //}else { // create new user
+                //db.collection('account').insertOne({ authID: profile.id }).save()
+                //.then((newUser) => {
+                    //console.log('new user created:'+ newUser);
+                    //done(null, newUser);
+                //});
+            //}
+        //}
+        //return done(null, profile);
+        //)}
+//));
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:5500/google/callback"
     },
+
+
     function(accessToken, refreshToken, profile, done) {
         // check if user is already exist in db
-        db.collection('account').findOne({authID: profile.id}).then((currentUser) => {
-            if (currentUser) {
-                console.log('user is:', currentUser);
-                done(null, currentUser);
+        //connects to the db
+        client
+        .connect()
+        .then((response) => {
+            const database = response.db("newMongoDB");
+            const collection = database.collection("users");
+
+        try {
+            let user = collection.findOne({authID: profile.id});
+            if (user) {
+                console.log('user is:', user);
+                done(null, user);
             }else { // create new user
-                db.collection('account').insertOne({ authID: profile.id }).save()
-                .then((newUser) => {
-                    console.log('new user created:'+ newUser);
-                    done(null, newUser);
-                });
+                user = collection.insertOne({ authID: profile.id })
+                console.log('new user created:'+ newUser);
+                done(null, user);
             }
+        } catch (err) {
+            console.error(err);
         }
-        //return done(null, profile);
-        )}
+        }
+    )}
 ));
+
 
 passport.serializeUser(function(user, done) {
     done(null, user);
