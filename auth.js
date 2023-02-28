53adb2a2d5bc2b9c763660d25057ceed0878535b
ngotruc999 <ngotruc@pdx.edu>
2023-02-28 13:36:31 -0800

    Update auth.js
    added uuid and change collection to blurp

--- auth.js
+++ auth.js
@@ -1,65 +1,42 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { client } = require("./routes/dbhandler")
+const crypto = require("crypto");
+
 
 const GOOGLE_CLIENT_ID = '220935592619-e7j93usk2h7vhcuoauos59rhgvqlcmsa.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = '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';
-
-//passport.use(new GoogleStrategy({
-    //clientID: GOOGLE_CLIENT_ID,
-    //clientSecret: GOOGLE_CLIENT_SECRET,
-    //callbackURL: "http://localhost:5500/google/callback"
-    //},
-    //function(accessToken, refreshToken, profile, done) {
-        // check if user is already exist in db
-        //db.collection('account').findOne({authID: profile.id}).then((currentUser) => {
-            //if (currentUser) {
-                //console.log('user is:', currentUser);
-                //done(null, currentUser);
-            //}else { // create new user
-                //db.collection('account').insertOne({ authID: profile.id }).save()
-                //.then((newUser) => {
-                    //console.log('new user created:'+ newUser);
-                    //done(null, newUser);
-                //});
-            //}
-        //}
-        //return done(null, profile);
-        //)}
-//));
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:5500/google/callback"
     },
-
-
     function(accessToken, refreshToken, profile, done) {
         // check if user is already exist in db
-        //connects to the db
         client
         .connect()
         .then((response) => {
-            const database = response.db("newMongoDB");
+            const database = response.db("blurp");
             const collection = database.collection("users");
 
-        try {
-            let user = collection.findOne({authID: profile.id});
+        collection.findOne({authID: profile.id}).then((user) => {
             if (user) {
                 console.log('user is:', user);
-                done(null, user);
+                cb(null, profile);
             }else { // create new user
-                user = collection.insertOne({ authID: profile.id })
-                console.log('new user created:'+ newUser);
-                done(null, user);
+                collection.insertOne({ authID: profile.id, UUID: crypto.randomUUID() })
+                .then((user) => {
+                    console.log('new user created:'+ user);
+                    cb(null, profile);
+                });
             }
-        } catch (err) {
-            console.error(err);
         }
-        }
-    )}
+        //return done(null, profile);
+        )})}
 ));
+
+
 
 
 passport.serializeUser(function(user, done) {
