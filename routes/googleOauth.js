bc3427f64627f3e18d039eccfa7a97137a0d2991
ngotruc999 <ngotruc@pdx.edu>
2023-02-28 15:27:09 -0800

    organize codes
    cleaning up so that it looks better

--- routes/googleOauth.js
+++ routes/googleOauth.js
@@ -2,11 +2,56 @@ const session = require('express-session');
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-require('../auth');
+const { client } = require("./dbhandler");
+const crypto = require("crypto");
 
 router.use(express({ secret: 'cats'}));
 router.use(passport.initialize());
 router.use(passport.session());
+
+const GOOGLE_CLIENT_ID = '220935592619-e7j93usk2h7vhcuoauos59rhgvqlcmsa.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5500/google/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // check if user is already exist in db
+        // connect to the db
+        client
+        .connect()
+        .then((response) => {
+            const database = response.db("blurp");
+            const collection = database.collection("users");
+        // check if user is already exist in db
+        collection.findOne({authID: profile.id}).then((user) => {
+            if (user) {
+                console.log('user is:', user);
+                cb(null, profile);
+            }else { // create new user
+                collection.insertOne({ authID: profile.id, userID: crypto.randomUUID() })
+                .then((user) => {
+                    console.log('new user created:'+ user);
+                    cb(null, profile);
+                });
+            }
+        }
+        //return done(null, profile);
+        )})}
+));
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
 
 
 function isLoggedIn(req, res, next) {
