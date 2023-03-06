a061e4f3f17c6b8015ff84ba13ab44dc0160ff14
Timofey Shichalin <76839122+timshicha@users.noreply.github.com>
2023-03-05 22:37:21 -0800

    adds UUID for each user that logs in and saves them to the DB

--- routes/login.js
+++ routes/login.js
@@ -12,6 +12,51 @@ router.use(cors({credentials: true, origin: FRONTEND_URL}));
 // Login session lasts for 1 month
 let SESSION_MAX_AGE = 30 * 24 * 3600;
 
+// Provide googleID, get userID (UUID) as promise.
+// The googleID is the key to the userID.
+function get_userID (googleID) {
+	return new Promise((resolve, reject) => {
+		// console.log('GOOGLEID in promise:', googleID);
+		let userID = null;
+		client.connect().then((res) => {
+			const database = res.db("blurp");
+			const collection = database.collection("users");
+
+			// console.log("User connected to DB");
+			// Search for userID using googleID
+			collection.findOne({googleID: googleID}).then((user) => {
+				// console.log("GOT HERE");
+				// If user already exists
+				if(user) {
+					userID = user.userID;
+					// console.log("User exists");
+					resolve(userID);
+					// return userID;
+				}
+				else {
+					// console.log("User does not exist");
+					userID = crypto.randomUUID();
+					collection
+					.insertOne({googleID: googleID, userID: userID})
+					.then((user) => {
+						// console.log("New user created.");
+						resolve(userID);
+						// return userID;
+					})
+					.catch((err) => {
+						resolve(null);
+						// return null;
+					});
+				}
+			});
+		}).catch((err) => {
+			// console.log("Error connecting to DB");
+			resolve(null);
+			// return null;
+		});
+	});
+}
+
 router.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", FRONTEND_URL);
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -36,26 +81,38 @@ router.get("/google", cors({origin: FRONTEND_URL}), (req, res, next) => {
 	fetch(url, {
 		headers: headers
 	}).then(result => result.json()).then((result) => {
-		console.log('\n\n\n\nGOOGLE RETURNED:', result);
+		// console.log('\n\n\n\nGOOGLE RETURNED:', result);
 		googleID = result.id;
 		req.session.cookie.maxAge = SESSION_MAX_AGE * 1000;
-		if(googleID == undefined) {
+		if(googleID == undefined || googleID == null) {
 			res.json({
 				'success': false
 			});
 		}
-		else 
-		{
-			res.json({
-				'success': true,
-				'userName': result.given_name,
-				'profileUrl': result.picture,
-				'maxAge': SESSION_MAX_AGE,
+		else {
+			// If successful, retrieve user from database (or add them)
+			get_userID(googleID).then((userID) => {
+				console.log('RESOLVED:', userID);
+				console.log('RETRIEVED USERID:', userID);
+				// If couldn't find and couldn't create, error
+				if(userID == null) {
+					res.json({
+						'success': false
+					});
+				}
+				else {
+					res.json({
+						'success': true,
+						'userName': result.given_name,
+						'profileUrl': result.picture,
+						'maxAge': SESSION_MAX_AGE,
+					});
+				}
 			});
 		}
 	})
 	.catch((err) => {
-		console.log('\n\n\nERROR');
+		// console.log('\n\n\nERROR');
 		res.json({
 			'success': false
 		});
